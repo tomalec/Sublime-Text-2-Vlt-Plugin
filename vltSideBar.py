26b23b89f83d8d7291c51f2f91fb5f0645311807
tomalec <tomalecpub@gmail.com>
2013-06-14 19:16:09 +0200

    New SideBar commands: status, remove&commit

--- vltSideBar.py
+++ vltSideBar.py
@@ -37,7 +37,29 @@ class VltSideBarCommitCommand(vlt.VltWindowCommand):
 class VltSideBarRemoveCommand(vlt.VltWindowCommand):
     def run(self, paths = []):
         self.run_command(['vlt', 'rm'] + paths , self.scratch, 
-            True, status_message="removing...", title="Vlt Remove", 
+            True, status_message="Removing...", title="Vlt Remove", 
+            syntax=vlt.plugin_file("syntax/Vlt Status.tmLanguage"))
+
+    def is_enabled(self, paths = []):
+        return True
+
+class VltSideBarRemoveAndCommitCommand(vlt.VltWindowCommand):
+    def run(self, paths = []):
+        self.run_command(['vlt', 'rm'] + paths , self.commit, 
+            True, status_message="Removing...")
+
+    def commit(self, paths = [], **kwargs):
+        self.run_command(['vlt', 'commit'] + paths , self.scratch, 
+            True, status_message="Commiting...", title="Vlt Remove", 
+            syntax=vlt.plugin_file("syntax/Vlt Status.tmLanguage"))
+
+    def is_enabled(self, paths = []):
+        return True
+
+class VltSideBarStatusCommand(vlt.VltWindowCommand):
+    def run(self, paths = []):
+        self.run_command(['vlt', 'st'] + paths , self.scratch, 
+            True, status_message="Checking status...", title="Vlt Status", 
             syntax=vlt.plugin_file("syntax/Vlt Status.tmLanguage"))
 
     def is_enabled(self, paths = []):
